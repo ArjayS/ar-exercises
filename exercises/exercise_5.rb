842ecec62aafab27cb476577f7a346c66b6d9fa2
Robert Servado <servadorobertgmail.com>
2022-10-25 17:06:47 -0400

    Modified exercise_5.rb

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum("annual_revenue")
+
+puts "The total revenue is #{@total_revenue}"
+
+@average_revenue = Store.average("annual_revenue")
+
+puts "The average revenue is #{@average_revenue}"
+
+@profitable_stores = Store.where("annual_revenue > 1000000").count
+
+puts "The number of profitable stores are #{@profitable_stores}"
