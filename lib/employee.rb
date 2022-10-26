f1a592bacd8df332d78de1c4f12de69c99d65a76
Robert Servado <servadorobertgmail.com>
2022-10-25 22:45:02 -0400

    Modified employees.rb

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
+  validates :first_name, :last_name, :store_id 
+  
+  # validates :name, presence: true, length: { minimum: 3 }
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 0 }, format: { with: /\A[+-]?\d+\z/ }
+
 end
