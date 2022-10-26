79aa3b4f5e8e682cfb1889ffee65862d6054c87d
Robert Servado <servadorobertgmail.com>
2022-10-25 22:44:33 -0400

    Modified exercise_7.rb attempted to finish but gotten an error for validating

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts 'Please enter your store name'
+print '> '
+store_name = $stdin.gets.chomp
+puts store_name.to_s
+user_store = Store.create(name: store_name, annual_revenue:, mens_apparel:, womens_apparel:).valid?
+
+puts user_store.errors.messages
