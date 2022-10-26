ef3ff0ee9351bd2c9b2f3834b59c94e176df5566
Robert Servado <servadorobertgmail.com>
2022-10-25 22:45:20 -0400

    Modified store.rb

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
+  # validates :name, presence: true, length: { minimum: 3 }
+  validates :name, length: { minimum: 3 }
 
+  validates :annual_revenue, numericality: { minimum: 3 }, format: { with: /\A[+-]?\d+\z/ }
+
+  validates :at_least_one_of_men_or_women_apparel
+
+  def at_least_one_of_men_or_women_apparel
+    if mens_apparel || womens_apparel
+      puts "There must at least one of men's or women's apparel"
+    end
+  end
 end
